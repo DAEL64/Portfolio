c8760bad877c1370b613a3f5669b338beeb1695e
Andria <jabajnldz@gmail.com>
2025-10-13 21:34:51 +0400

    finished about me page. added text and animations. optimized site performance. fixed bugs.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import "../styles/about.css";
+import ScrambledText from "../animation_hooks/ScrambledText";
+
 
 export default function About() {
   useEffect(() => {
@@ -25,10 +27,31 @@ export default function About() {
           <div className="w-full flex justify-center text-5xl font-medium">
             <p>About me</p>
           </div>
-          <div className="w-6/7 flex justify-end items-center gap-5 mt-40">
+          <div className="w-full flex justify-center  items-center gap-5 mt-40">
             <div className="cardWrap">
-              <div className="aboutCard bg-stone-900 rounded-md min-w-96 min-h-80 flex p-2">
-                <p>Hello, I am Andria</p>
+              <div className="aboutCard bg-stone-900 max-w-150 rounded-md min-w-96 min-h-80 flex p-2">
+                <ScrambledText
+                  radius={100}
+                  duration={1}
+                  speed={0.5}
+                  scrambleChars=".:"
+                >
+                  <span className="line-clamp-1">Hi! I am <span className="text-amber-500">Andria Janelidze</span>, a front-end developer from Georgia,
+                  born in 2006. With several years of experience in JavaScript,
+                  HTML, CSS, and React.js/TSX, I specialize in crafting modern,
+                  efficient, and visually engaging web experiences. My goal is
+                  to create websites that not only meet but exceed client
+                  expectations, aligning perfectly with their unique vision and
+                  requirements. I place great emphasis on building responsive,
+                  user-friendly interfaces that deliver seamless interactions
+                  across all devices. Driven by a passion for clean design,
+                  intuitive UX, and high-performance code, I continuously refine
+                  my skills and stay up to date with the latest trends in
+                  front-end development. For me, every project is an opportunity
+                  to combine technical precision with aesthetic quality,
+                  creating digital experiences that are both functional and
+                  inspiring.</span>
+                </ScrambledText>
               </div>
             </div>
           </div>
